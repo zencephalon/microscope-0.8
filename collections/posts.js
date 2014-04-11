3bfbef28a9b858b1f5c0c448a1e3d06d0944e5fa
Matthew Bunday <zenceph@gmail.com>
2014-04-11 16:01:55 -0400

    Only allow editing url and title.

--- collections/posts.js
+++ collections/posts.js
@@ -4,6 +4,13 @@ Posts.allow({
   remove: ownsDocument
 });
 
+Posts.deny({
+  update: function(userId, post, fieldNames) {
+    // may only edit the following two fields:
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
+});
+
 Meteor.methods({
   post: function(postAttributes) {
     var user = Meteor.user(),
